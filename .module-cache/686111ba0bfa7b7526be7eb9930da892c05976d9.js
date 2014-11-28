3d39950cd5fe13cba136f1358dddf860e92b5c89
Mukesh Soni <mukeshsoni@gmail.com>
2014-11-28 16:55:54 +0530

    Remove the now ignored module-cache directory

--- .module-cache/686111ba0bfa7b7526be7eb9930da892c05976d9.js
+++
@@ -1,6 +0,0 @@
-require('./main')
-
-React.renderComponent(
-  React.createElement("h1", null, "Hello, world!"),
-  document.getElementById('example')
-);
--- .module-cache/cd0fb94b71afe679e486a4e810a1e436aa3041bb.js
+++
@@ -1,1 +0,0 @@
-var func = (x) => React.createElement("div", null, x);
