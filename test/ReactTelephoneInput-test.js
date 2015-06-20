b502c1f7a556927b9bc0954131b62c35b92e1260
Mukesh Soni <mukeshsoni@gmail.com>
2015-06-20 20:27:50 +0530

    changed the test setup to use mochify. The previous one won't work since we are also requiring less code and doing jsx transformation

--- test/ReactTelephoneInput-test.js
+++ test/ReactTelephoneInput-test.js
@@ -1,15 +1,16 @@
 'use strict';
 /* global describe, it, beforeEach */
 
-var jsdom = require('mocha-jsdom');
 var expect = require('chai').expect;
 var React = require('react/addons');
 var TestUtils = React.addons.TestUtils;
+var ReactTelephoneInput = require('../src/index.js');
 
 describe('react telephone input', function() {
-    jsdom();
-
-    it('should run example test', function() {
+    it('mandatory existential crisis test', function() {
+        var rti = TestUtils.renderIntoDocument(React.createElement(ReactTelephoneInput, {}));
+        expect(rti).to.be.defined;
+        expect(rti.refs.numberInput).to.be.defined;
         expect(true).to.be.true;
     });
 });
