cab75f05c0126efce220c9e23e1310b5addefd81
Mukesh Soni <mukeshsoni@gmail.com>
2015-06-20 16:58:04 +0530

    added gulpfile.js file

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+var gulp = require('gulp'),
+    initGulpTasks = require('react-component-gulp-tasks'),
+
+// Read the package.json to detect the package name and dependencies
+var pkg = JSON.parse(require('fs').readFileSync('./package.json'));
+
+var taskConfig = {
+
+    component: {
+        file: 'index.js',
+        // The component name controls the standalone module name
+        name: 'ReactTelephoneInput',
+        src: 'src',
+        dist: 'dist',
+        dependencies: [
+            'classnames',
+            'react',
+            'react/addons'
+        ],
+        lib: 'lib',
+        // This is the name of the package that will be exported
+        // by the component file. It must match the name of your
+        // package on npm
+        pkgName: pkg.name,
+        less: {
+            path: 'less',
+            entry: 'default.less'
+        }
+    },
+
+    example: {
+        src: 'example/src',
+        dist: 'example/dist',
+        files: [
+            'index.html',
+            'flags.png',
+            '.gitignore'
+        ],
+        scripts: [
+            'example.js'
+        ],
+        less: [
+            'styles.less'
+        ]
+    }
+
+};
+
+initGulpTasks(gulp, config);
