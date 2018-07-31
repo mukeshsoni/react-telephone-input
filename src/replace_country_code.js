513507afcb36ad4d56bcc89596bd9252774fca2b
Luis Villamil <luis.villamil@alli-mc-180710a.ad.unosquare.com>
2018-07-31 17:36:48 -0500

    Remove automatic country code appending

--- src/replace_country_code.js
+++ src/replace_country_code.js
@@ -5,10 +5,5 @@ export default function replaceCountryCode(
 ) {
   const dialCodeRegex = RegExp(`^(${currentSelectedCountry.dialCode})`)
   const newNumber = number.replace(dialCodeRegex, nextSelectedCountry.dialCode)
-
-  // if we couldn't find any replacement, just attach the new country's dial code at the front
-  if (newNumber === number) {
-    return nextSelectedCountry.dialCode + number
-  }
   return newNumber
 }
