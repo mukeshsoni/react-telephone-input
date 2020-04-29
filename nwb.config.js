eb6a7a516a89162e1b4cdc390a956e99793efd52
Mukesh Soni <mukeshsoni@gmail.com>
2020-04-29 22:47:29 +0530

    fix build problems

--- nwb.config.js
+++ nwb.config.js
@@ -1,16 +1,9 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = {
   webpack: {
     aliases: {
-      react: path.resolve(__dirname, './node_modules', 'react')
-    }
+      react: path.resolve(__dirname, './node_modules', 'react'),
+    },
   },
-  babel: {
-    env: {
-      production: {
-        plugins: ['babel-plugin-remove-data-test-id-attribute']
-      }
-    }
-  }
-}
+};
