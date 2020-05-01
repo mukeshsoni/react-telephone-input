9c3e3efdfa35066d5fee67d2ad0de45e9ebe1b25
Mukesh Soni <mukeshsoni@gmail.com>
2020-05-01 07:43:17 +0530

    update demos

--- demo/src/index.js
+++ demo/src/index.js
@@ -8,26 +8,40 @@ import RTI from '../../src/withStyles';
 const flagsImagePath = require('../../images/flags.png');
 // const RTI = testGenerator(ReactTelephoneInput)
 
-const propsDemo3 = {
-  preferredCountries: ['af', 'al'],
-  defaultCountry: 'in',
-  flagsImagePath: '/flags.723494a4.png',
-  initialValue: '+9112121',
-  inputProps: {
-    autoFocus: true,
-  },
-};
+// eslint-disable-next-line
+function CodeBlock({ children }) {
+  return (
+    <div style={{ marginBottom: 10 }}>
+      <pre>
+        <code>{children}</code>
+      </pre>
+    </div>
+  );
+}
 
 const Demo = () => (
   <div>
-    <h1>
+    <h4>
       Github repo -{' '}
       <a href="https://github.com/mukeshsoni/react-telephone-input">
         https://github.com/mukeshsoni/react-telephone-input
       </a>
-    </h1>
+    </h4>
     <h1>React Telephone Input Demo</h1>
-    <h4>Base case</h4>
+    <h3>Base case</h3>
+    <CodeBlock>{'<RTI flagsImagePath={flagsImagePath} />'}</CodeBlock>
+    <RTI flagsImagePath={flagsImagePath} />
+    <br />
+    <h3>With initial values and auto focus</h3>
+    <CodeBlock>
+      {`<RTI
+  preferredCountries={['af', 'al']}
+  defaultCountry="in"
+  flagsImagePath={flagsImagePath}
+  initialValue="+9112121"
+  inputProps={{ autoFocus: true }}
+/>`}
+    </CodeBlock>
     <RTI
       preferredCountries={['af', 'al']}
       defaultCountry="in"
@@ -35,38 +49,51 @@ const Demo = () => (
       initialValue="+9112121"
       inputProps={{ autoFocus: true }}
     />
+    <br />
+    <CodeBlock>
+      {`<RTI
+  defaultCountry="us"
+  preferredCountries={['us', 'ca', 'zz', 'hk']}
+  flagsImagePath={flagsImagePath}
+/>`}
+    </CodeBlock>
     <RTI
       defaultCountry="us"
       preferredCountries={['us', 'ca', 'zz', 'hk']}
       flagsImagePath={flagsImagePath}
     />
-    <RTI {...propsDemo3} />
-    {/* <h4>With initial value</h4>
-      <RTI
-        defaultCountry="in"
-        flagsImagePath={flagsImagePath}
-        initialValue="+9112121"
-      />
-      <h4>With initial value and autoFocus on input</h4>
-      <RTI
-        defaultCountry="in"
-        flagsImagePath={flagsImagePath}
-        initialValue="+9112121"
-        inputProps={{ autoFocus: true }}
-      />
-      <h4>Different country - Albania</h4>
-      <RTI
-        defaultCountry="al"
-        flagsImagePath={flagsImagePath}
-        initialValue="3559112121"
-      />
-      <h4>With preferred countries</h4>
-      <RTI
-        defaultCountry="us"
-        flagsImagePath={flagsImagePath}
-        initialValue="+13559112121"
-        preferredCountries={['us', 'ca', 'zz', 'hk']}
-      /> */}
+    <br />
+    <h3>Different country - Albania</h3>
+    <CodeBlock>
+      {`<RTI
+  flagsImagePath={flagsImagePath}
+  preferredCountries={['af', 'al']}
+  defaultCountry="in"
+  initialValue="+9112121"
+/>`}
+    </CodeBlock>
+    <RTI
+      flagsImagePath={flagsImagePath}
+      preferredCountries={['af', 'al']}
+      defaultCountry="in"
+      initialValue="+9112121"
+    />
+    <br />
+    <h3>With preferred countries</h3>
+    <CodeBlock>
+      {`<RTI
+  defaultCountry="us"
+  flagsImagePath={flagsImagePath}
+  initialValue="+13559112121"
+  preferredCountries={['us', 'ca', 'zz', 'hk']}
+/>`}
+    </CodeBlock>
+    <RTI
+      defaultCountry="us"
+      flagsImagePath={flagsImagePath}
+      initialValue="+13559112121"
+      preferredCountries={['us', 'ca', 'zz', 'hk']}
+    />
   </div>
 );
 
