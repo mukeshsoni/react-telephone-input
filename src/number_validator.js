6e53d5d4d1ad2405101a046804b3ceab982ab825
Mukesh Soni <mukeshsoni@gmail.com>
2020-05-01 21:28:53 +0530

    converted the format number function to typescript

--- src/number_validator.js
+++
@@ -1,9 +0,0 @@
-import R from 'cramda'
-import countryData from 'country-telephone-data'
-
-export default function isNumberValid(inputNumber) {
-  const countries = countryData.allCountries
-  return R.any(country => (R.startsWith(country.dialCode, inputNumber)
-    || R.startsWith(inputNumber, country.dialCode)
-  ), countries)
-}
